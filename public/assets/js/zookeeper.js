38d2048c910f14624af11d6c2ed5bca6857b6e93
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-07-19 22:39:06 -0700

    add query to get zookeepers

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,4 +1,5 @@
 const $displayArea = document.querySelector('#display-area');
+const $zookeeperForm = document.querySelector('#zookeeper-form');
 
 
 const printResults = resultArr => {
@@ -21,8 +22,13 @@ const printResults = resultArr => {
   $displayArea.innerHTML = animalHTML.join('');
 };
 
-const getZookeepers = () => {
-  fetch('/api/zookeepers')
+const getZookeepers = (formData = {}) => {
+  let queryUrl = '/api/zookeepers?';
+
+  Object.entries(formData).forEach(([key, value]) => {
+    queryUrl += `${key}=${value}&`
+  })
+  fetch(queryUrl)
     .then(response => {
       if (!response.ok) {
         return alert('Error: ' + response.statusText);
@@ -35,4 +41,22 @@ const getZookeepers = () => {
     });
 };
 
+const handleGetZookeepersSubmit = event => {
+  event.preventDefault();
+  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
+  const name = nameHTML.value;
+  nameHTML.value = "";
+
+  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
+  const age = ageHTML.value;
+  ageHTML.value = "";
+  const zookeeperObject = { name, age };
+
+  getZookeepers(zookeeperObject);
+}
+
+$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
+
 getZookeepers();
+
+
